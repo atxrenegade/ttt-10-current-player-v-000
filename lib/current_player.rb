7e36132c544c6774f720c24bd8c901bc88303b5d
Harleigh Abel <harleighabel@gmail.com>
2018-01-24 21:24:51 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,17 @@
-def turn_count
+def turn_count(board)
+  counter = 0
+  turn = 0
+  board[counter].each {|counter|  }
+  if board[counter] == "X" || board[counter] == "O"
+    turn 1
+  end  
 
 end
 
-def current_player
-
-end    
+def current_player(board)
+  count = turn_count(board)
+  if count % 2 || count == 0
+    return "X"
+  else
+    return "O"  
+end
